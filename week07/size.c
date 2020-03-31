22087c795680df4d15b98fdc87c4fd9bdebfd91d
Claire <claire.hua@unsw.edu.au>
2020-03-31 17:08:28 +1100

    uploaded week 7 content

---
+++ week07/size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+
+    printf("the size of an int is %lu bytes\n", sizeof(int));
+    printf("the size of a double is %lu bytes\n", sizeof(double));
+    printf("the size of a char is %lu bytes\n", sizeof(char));
+    return 0;
+}
